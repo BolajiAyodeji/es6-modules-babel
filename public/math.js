2c0d81124db7f4bd820ce0722d9ee8165884c33e
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-02-13 07:36:44 +0100

    refactor :zap:

--- public/math.js
+++ public/math.js
@@ -1,36 +1,19 @@
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.findModulus = exports.multiplyAll = exports.divideAll = exports.subtractAll = exports.sumAll = void 0;
-
-var sumAll = function sumAll(a, b) {
-  return a + b;
+module.exports = {
+  sumAll: function sumAll(a, b) {
+    return a + b;
+  },
+  subtractAll: function subtractAll(a, b) {
+    return a - b;
+  },
+  multiplyAll: function multiplyAll(a, b) {
+    return a * b;
+  },
+  divideAll: function divideAll(a, b) {
+    return a / b;
+  },
+  findModulus: function findModulus(a, b) {
+    return a % b;
+  }
 };
-
-exports.sumAll = sumAll;
-
-var subtractAll = function subtractAll(a, b) {
-  return a - b;
-};
-
-exports.subtractAll = subtractAll;
-
-var divideAll = function divideAll(a, b) {
-  return a / b;
-};
-
-exports.divideAll = divideAll;
-
-var multiplyAll = function multiplyAll(a, b) {
-  return a * b;
-};
-
-exports.multiplyAll = multiplyAll;
-
-var findModulus = function findModulus(a, b) {
-  return a % b;
-};
-
-exports.findModulus = findModulus;
